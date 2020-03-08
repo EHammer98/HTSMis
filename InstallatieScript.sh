8b8e31e78f327df3a700319ac638fc37f46c9c88
Elwin Hammer <elwinhammer@gmail.com>
2020-03-08 15:44:43 +0100

    Update InstallatieScript.sh

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -6,13 +6,18 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 echo -e "${BLUE}HammerTECH automatisch installatie script... ${GRN}[${V}]${BLUE}"
 echo "Laatste updates installeren..."
-apt-get update --assume-yes
-apt-get upgrade --assume-yes
-apt-get install curl
+sudo apt-get update --assume-yes
+sudo apt-get upgrade --assume-yes
+sudo apt-get install curl
 echo -e "${BLUE}Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${BLUE}"
 bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" -y
 echo -e "${BLUE}avoid_warnings=1" >>  /boot/config.txt
-apt-get install unclutter --assume-yes
+sudo apt-get install unclutter --assume-yes
+echo "Extra modules installeren
+cd ~/MagicMirror/modules 
+git clone https://github.com/htilburgs/MMM-MyTraffic
+git clone https://github.com/Taolanoz/MMM-RSS-FEED
+git clone https://github.com/73cirdan/MMM-rainfc
 echo "Opnieuw opstarten..."
 reboot
 #etc.
