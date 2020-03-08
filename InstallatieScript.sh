574e94e17ae71bef3c0aa1ff16699e76c5cd0568
Elwin Hammer <elwinhammer@gmail.com>
2020-03-08 15:26:53 +0100

    HTSMis V.0.0.3

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -1,30 +1,17 @@
 #!/bin/sh
+V='0.0.3'
+RED='\033[0;31m'
+GRN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
-echo -e "${BLUE}HammerTECH automatisch installatie script... Even geduld a.u.b., uw systeem kan meerdere malen opnieuw opstarten."
-echo -e "${BLUE}Laatste updates installere..."
-apt-get update -y
-apt-get upgrade -y
+echo -e "${BLUE}HammerTECH automatisch installatie script... ${GRN}[&{V}]${BLUE}"
+echo "Laatste updates installeren..."
+apt-get update --yes
+apt-get upgrade --yes
 apt-get install curl
-echo "Klaar, MagicMirror installeren..."
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-apt-get install -y nodejs
-git clone https://github.com/MichMich/MagicMirror
-cd MagicMirror/
-npm install -g electron@6.0.12
-echo "Klaar, automatisch MagicMirror opstarten inschakelen"
-pm2 startup
-cd ~
-echo "cd ~/MagicMirror" >> mm.sh
-echo "DISPLAY=:0 npm start" >> mm.sh
-chmod +x mm.sh
-pm2 start mm.sh
-pm2 save
-echo "Klaar, laatste dingen inorde maken..."
-echo "@xset s noblank" >>  ~/.config/lxsession/LXDE/autostart
-echo "@xset s off" >>  ~/.config/lxsession/LXDE/autostart
-echo "@xset s -dpms" >>  ~/.config/lxsession/LXDE/autostart
-echo "avoid_warnings=1" >>  /boot/config.txt
+echo -e "${BLUE}Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${BLUE}"
+bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" -y
+echo -e "${BLUE}avoid_warnings=1" >>  /boot/config.txt
 apt-get install unclutter
 echo "Opnieuw opstarten..."
 reboot
