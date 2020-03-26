040a79f922cd564a49fcd317cfbed1c23abeb54a
Elwin Hammer <elwinhammer@gmail.com>
2020-03-26 21:51:58 +0100

    Update InstallatieScript.sh

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 V='Versie: 0.1.0'
-RED='\033[0;31m'
-GRN='\033[0;32m'
-BLUE='\033[0;34m'
-BOLD='\e[1m'
-NC='\033[0m' 
-echo -e "${BOLD}${BLUE}HammerTECH automatisch installatie script... ";" ${GRN} ${V} ${BLUE}";
+#RED='\033[0;31m'
+#GRN='\033[0;32m'
+#BLUE='\033[0;34m'
+#BOLD='\e[1m'
+#NC='\033[0m' 
+echo -e "HammerTECH automatisch installatie script... ";"${V}";
 echo "Laatste updates installeren...\n"
 apt update
 apt upgrade
-echo -e "${BLUE}Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${BLUE}";
+echo "Klaar, MagicMirror installeren... INTERACTIE VEREIST!";
 #bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
 echo -e "${BLUE}avoid_warnings=1" >>  /boot/config.txt
 sudo apt-get install unclutter 
@@ -21,17 +21,17 @@ git clone https://github.com/73cirdan/MMM-rainfc
 git clone https://github.com/eouia/MMM-Remote-Control-Repository
 #bash -c "$(curl -sL https://raw.githubusercontent.com/Jopyth/MMM-Remote-Control/master/installer.sh)" 
 echo "Wachtwoord veranderen van pi-user";
-echo -e "${RED}BELANGRIJK! ${BLUE} Noteer het volgende als Pi wachtwoord: ${GRN}";
+echo "BELANGRIJK! Noteer het volgende als Pi wachtwoord: ";
 echo date +%s | sha256sum | base64 | head -c 16 > test1.txt
 tmp=$(<test1.txt)
 echo "$value";
 yes $tmp | sudo passwd pi
 rm test1.txt
-echo -e "${RED}BELANGRIJK! ${BLUE} Noteer het volgende als Pi MAC-adres: ${GRN}";
+echo "BELANGRIJK! Noteer het volgende als Pi MAC-adres: ";
 IFACE=wlan0
 read MAC </sys/class/net/$IFACE/address
 echo "$IFACE $MAC" ;
-echo -e "${BLUE} SSH aanzetten...";
+echo "SSH aanzetten...";
 sudo systemctl enable ssh 
 sudo systemctl start ssh 
 echo "WiFiSetup installeren en uitvoeren...";
