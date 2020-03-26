17fce2ebb32752b96ae8ca7c5a7e2b7adacbcf8a
Elwin Hammer <elwinhammer@gmail.com>
2020-03-26 22:32:45 +0100

    V0.1.2
    Full func. script

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-V='Versie: 0.1.1'
+V='Versie: 0.1.2'
 RED='\033[0;31m'
 GRN='\033[0;32m'
 BLUE='\033[0;34m'
@@ -10,7 +10,7 @@ echo "Laatste updates installeren...";
 apt update
 apt upgrade
 echo -e "Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${NC}";
-#bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
+bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
 echo "avoid_warnings=1" >>  /boot/config.txt
 sudo apt-get install unclutter 
 echo "Extra modules installeren";
@@ -19,7 +19,7 @@ git clone https://github.com/htilburgs/MMM-MyTraffic
 git clone https://github.com/Taolanoz/MMM-RSS-FEED
 git clone https://github.com/73cirdan/MMM-rainfc
 git clone https://github.com/eouia/MMM-Remote-Control-Repository
-#bash -c "$(curl -sL https://raw.githubusercontent.com/Jopyth/MMM-Remote-Control/master/installer.sh)" 
+bash -c "$(curl -sL https://raw.githubusercontent.com/Jopyth/MMM-Remote-Control/master/installer.sh)" 
 echo "Wachtwoord veranderen van pi-user";
 echo -e "${RED}BELANGRIJK! ${NC} Noteer het volgende als Pi wachtwoord: ${GRN}";
 echo date +%s | sha256sum | base64 | head -c 16 > test1.txt
@@ -42,7 +42,8 @@ wget "https://github.com/EHammer98/HTSMis/blob/master/WiFiSetup.zip"
 unzip WiFiSetup.zip 
 cd ~/WiFiSetup
 sudo apt-get install -y python3-pip 
-sudo python3 setup_lib.py
+#sudo python3 setup_lib.py
+sudo python3 initial_setup.py
 echo "Opnieuw opstarten...";
 shutdown -r now
 #etc.
