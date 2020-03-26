5b475c1eb19fdc3fe1dc7c67af2adbc322b9d183
Elwin Hammer <elwinhammer@gmail.com>
2020-03-26 21:16:48 +0100

    V0.0.7
    Mac-adres is getest binnen Debian en zou nu moeten werken

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
-V='Versie: 0.0.7'
+V='Versie: 0.0.8'
 RED='\033[0;31m'
 GRN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
-echo -e "${BLUE}HammerTECH automatisch installatie script... ${GRN} ${V} ${BLUE}\n"
+echo -e "${BLUE}HammerTECH automatisch installatie script... ${GRN} ${V} ${BLUE}";
 echo "Laatste updates installeren...\n"
 sudo apt update
 sudo apt upgrade
-echo -e "${BLUE}Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${BLUE}\n"
+echo -e "${BLUE}Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${BLUE}";
 #bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
 echo -e "${BLUE}avoid_warnings=1" >>  /boot/config.txt
 sudo apt-get install unclutter 
-echo "Extra modules installeren"
+echo "Extra modules installeren";
 cd ~/MagicMirror/modules 
 git clone https://github.com/htilburgs/MMM-MyTraffic
 git clone https://github.com/Taolanoz/MMM-RSS-FEED
 git clone https://github.com/73cirdan/MMM-rainfc
 git clone https://github.com/eouia/MMM-Remote-Control-Repository
 #bash -c "$(curl -sL https://raw.githubusercontent.com/Jopyth/MMM-Remote-Control/master/installer.sh)" 
-echo "Wachtwoord veranderen van pi-user"
-echo "${RED}BELANGRIJK! ${BLUE} Noteer het volgende als Pi wachtwoord: ${GRN}\n"
+echo "Wachtwoord veranderen van pi-user";
+echo "${RED}BELANGRIJK! ${BLUE} Noteer het volgende als Pi wachtwoord: ${GRN}";
 echo date +%s | sha256sum | base64 | head -c 16 > test1.txt
 tmp=$(<test1.txt)
-echo "$value"
+echo "$value";
 yes $tmp | sudo passwd pi
 rm test1.txt
-echo "${BLUE}"
-ifconfig -a |
-awk '/^[a-z]/ { iface=$1; mac=$NF; next }
-    /inet addr:/ { print iface, mac }'
-echo "SSH aanzetten..."
+echo "${RED}BELANGRIJK! ${BLUE} Noteer het volgende als Pi MAC-adres: ${GRN}";
+IFACE=wlan0
+read MAC </sys/class/net/$IFACE/address
+echo "$IFACE $MAC ;
+echo "${BLUE} SSH aanzetten...";
 sudo systemctl enable ssh 
 sudo systemctl start ssh 
-echo "WiFiSetup installeren en uitvoeren..."
+echo "WiFiSetup installeren en uitvoeren...";
 sudo apt-get install unzip 
 cd ~
 mkdir WiFiSetup
@@ -42,6 +42,6 @@ unzip WiFiSetup.zip
 cd ~/WiFiSetup
 sudo apt-get install -y python3-pip 
 sudo python3 setup_lib.py
-echo "Opnieuw opstarten..."
+echo "Opnieuw opstarten...";
 reboot
 #etc.
