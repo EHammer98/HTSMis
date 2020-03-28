ab0be117676684cff071c6e7053eb0a75cf25c48
Elwin Hammer <elwinhammer@gmail.com>
2020-03-28 14:39:42 +0100

    V.0.1.8

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-V='Versie: 0.1.7'
+V='Versie: 0.1.8'
 RED='\033[0;31m'
 GRN='\033[0;32m'
 BLUE='\033[0;34m'
 BOLD='\e[1m'
 NC='\033[0m' 
-echo -e "${BOLD}HammerTECH automatisch installatie script... ";" ${GRN} ${V} ${NC}";
+echo -e "${BOLD}HammerTECH automatisch installatie script...";
+echo -e "${GRN} ${V} ${NC}";
 echo "Laatste updates installeren...";
 #yes | apt update
 #yes | apt upgrade
 echo -e "Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${NC}";
-yes | bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
+yes | sudo bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
 echo "avoid_warnings=1" >>  /boot/config.txt
 echo "printf 'UPDATING...'
 sudo apt-get update -y
@@ -23,7 +24,7 @@ git clone https://github.com/htilburgs/MMM-MyTraffic
 git clone https://github.com/Taolanoz/MMM-RSS-FEED
 git clone https://github.com/73cirdan/MMM-rainfc
 git clone https://github.com/eouia/MMM-Remote-Control-Repository
-yes | bash -c "$(curl -sL https://raw.githubusercontent.com/Jopyth/MMM-Remote-Control/master/installer.sh)" 
+yes | sudo bash -c "$(curl -sL https://raw.githubusercontent.com/Jopyth/MMM-Remote-Control/master/installer.sh)" 
 echo "Wachtwoord veranderen van pi-user";
 echo -e "${RED}BELANGRIJK! ${NC} Noteer het volgende als Pi wachtwoord: ${GRN}";
 echo date +%s | sha256sum | base64 | head -c 16 > test1.txt
@@ -43,7 +44,7 @@ echo "WiFiSetup installeren en uitvoeren...";
 yes | sudo apt-get install unzip 
 mkdir /WiFiSetup
 curl -LO "https://github.com/EHammer98/HTSMis/raw/master/WiFiSetup.zip"
-sudo unzip WiFiSetup.zip -d /WiFiSetup
+yes | sudo unzip WiFiSetup.zip -d /WiFiSetup
 cd /WiFiSetup
 yes | sudo apt-get install -y python3-pip 
 sudo python3 setup_lib.py
