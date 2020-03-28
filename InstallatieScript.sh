3b4b3dc97650d6096eea876cef64e065996b6d57
Elwin Hammer <elwinhammer@gmail.com>
2020-03-28 18:55:18 +0100

    V0.2.2

--- InstallatieScript.sh
+++ InstallatieScript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh'
-V='0.2.1'
+V='0.2.2'
 RED='\033[0;31m'
 GRN='\033[0;32m'
 BLUE='\033[0;34m'
@@ -13,6 +13,7 @@ echo "Laatste updates installeren...";
 echo -e "Klaar, MagicMirror installeren... ${RED}INTERACTIE VEREIST!${NC}";
 yes | bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)" 
 echo "avoid_warnings=1" | sudo tee -a /boot/config.txt
+sudo rm /etc/rc.local
 echo "printf 'UPDATING...'
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -22,7 +23,7 @@ echo "@xset s noblank
 @xset s off
 @xset =dmps" | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 yes | sudo apt-get install unclutter 
-pm2 startup
+~/pm2 startup
 echo "cd ~/MagicMirror
 DISPLAY:0 npm start" >> ~/mm.sh
 sudo chmod +x ~/mm.sh
@@ -50,15 +51,15 @@ sudo systemctl start ssh
 echo "WiFiSetup installeren en uitvoeren...";
 yes | sudo apt-get install unzip 
 mkdir ~/WiFiSetup
-curl -L "https://github.com/EHammer98/HTSMis/raw/master/WiFiSetup.zip" > ~/WiFiSetup.zip
+sudo curl -L "https://github.com/EHammer98/HTSMis/raw/master/WiFiSetup.zip" > ~/WiFiSetup.zip
 yes | sudo unzip ~/WiFiSetup.zip -d ~/WiFiSetup
 cd ~/WiFiSetup
 yes | sudo apt-get install -y python3-pip 
 sudo python3 setup_lib.py
 #sudo python3 initial_setup.py
 echo "MM starten...";
-pm2 start ~/mm.sh
-pm2 save
+~/pm2 start ~/mm.sh
+~/pm2 save
 echo "Opnieuw opstarten...";
 #shutdown -r now
 #etc.
